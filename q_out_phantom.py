0e2b168889cde3cde5bda8ade26eecc32716e49a
Komsit Prakobphol <pkomsit@folio-sec.com>
2019-12-12 15:29:53 +0900

    properly show table in phantom, you can also browse table with shift+f2

--- q_out_phantom.py
+++ q_out_phantom.py
@@ -1,6 +1,9 @@
 import sublime, sublime_plugin
-from . import q_select_text
 from . import q_chain
+import ast
+from . import q_select_text
+from . import q_send
+from . import QCon
 
 #show output in phantom
 #see https://www.sublimetext.com/docs/3/api_reference.html#sublime.Phantom
@@ -9,52 +12,119 @@ class QOutPhantomCommand(q_chain.QChainCommand):
 
   def __init__(self, view):
     self.view = view
-    self.next_id = 0
-    self.view.settings().set('phantoms', [])
-
-  def get_next_id(self):
-    id = self.next_id
-    self.next_id = self.next_id + 1
-    return id
+    active_phantom_ids = self.view.settings().get('active_phantom_ids')
+    if not active_phantom_ids:
+      self.view.settings().set('active_phantom_ids', [])
 
   def do(self, edit, input=None):
-    id = str(self.get_next_id())
+    location = self.view.sel()[0].end()
+    id = str(location)
+    html = self.render_text_result(input, id)
+    self.add_phantom(html, id, location)
 
-    select_region = self.view.sel()[0]
-    end_selection_region = sublime.Region(select_region.end(), select_region.end())
+  def render_text_result(self, input, id):
+    inputx = input.split("\n")
+    c = len(inputx)
+    if c > 10:
+      inputx = inputx[0:10]
+      inputx.append("...({0} more)".format(c-10-1)) #-2 for header rows
+    inputx = "<br>".join(inputx)
 
-    template = '<a href="{0}" style="display:inline;color:grey;">x</a> {1}'
-    html = template.format(id, input)
+    template = '<a href="close[{0}" style="display:inline;color:grey;">x</a> {1}'
+    html = template.format(id, inputx)
+    return html
+
+  def add_phantom(self, html, id, location):
+    #remove old one first if exist
+    active_phantom_ids = self.view.settings().get('active_phantom_ids')
+    if id in active_phantom_ids:
+      self.view.erase_phantoms(id)
+    else:
+      active_phantom_ids.append(id)
+      self.view.settings().set('active_phantom_ids', active_phantom_ids)
 
     self.view.add_phantom(id,
-      end_selection_region,
+      sublime.Region(location),
       html,
       sublime.LAYOUT_BLOCK,
       self.on_navigate
     )
-    phantoms = self.view.settings().get('phantoms')
-    phantoms.append(id)
-    self.view.settings().set('phantoms', phantoms)
-    #self.phantom_set.update([])
 
-  def on_navigate(self, id):
-    phantoms = self.view.settings().get('phantoms')
-    phantoms.remove(id)
+  def remove_phantom(self, id):
+    active_phantom_ids = self.view.settings().get('active_phantom_ids')
+    active_phantom_ids.remove(id)
     self.view.erase_phantoms(id)
-    self.view.settings().set('phantoms', phantoms)
+    self.view.settings().set('active_phantom_ids', active_phantom_ids)
+
+  def on_navigate(self, href):
+    try:
+      x = href.split('[')
+      if x[0] == 'close':
+        id = x[1]
+        self.remove_phantom(id)
+        return True
+      #else:
+        #print("unknown href: {0}".format(href))
+    except Exception as e:
+      #print(active_phantom_ids)
+      #print(href)
+      sublime.error_message("Phatom navigation error\nactive_phantom_ids: {0}\nhref: {1}\nerror: {2}".format(active_phantom_ids, href, e))
+    return False
+
+class QBrowseTablePhantomCommand(QOutPhantomCommand):
+
+  def run(self, edit):
+    self.get_table(None, 0, self.view.sel()[0].end())
+
+  def get_table(self, table, index, location):
+    con = QCon.QCon.loadFromViewWithPrompt(self.view)
+    if con:
+      if not table:
+        table = q_select_text.QSelectWordCommand.selectWord(self.view)
+      statement = ".h.jx[{1}] `{0}".format(table, index)
+      res = q_send.QSendRawCommand.sendAndUpdateStatus(self.view, con, statement)
+      #print(res)
+
+      id = str(location)
+      html = self.render_html_result(res, id, table)
+      self.add_phantom(html, id, location)
+
+  def render_html_result(self, input, id, table):
+    inputx = [x.decode('utf-8') for x in ast.literal_eval(input)]
+    inputx[0] = inputx[0].replace('href="?[', 'href="?[{0}[{1}['.format(id, table))
+    inputx = "<br>".join(inputx)
+
+    template = '<a href="close[{0}" style="display:inline;color:grey;">x</a> {1}'
+    html = template.format(id, inputx)
+    return html
+
+  def on_navigate(self, href):
+    if not super().on_navigate(href):
+      try:
+        x = href.split('[')
+        if x[0] == '?':
+          id = x[1]
+          table = x[2]
+          index = x[3]
+          self.get_table(table, index, int(id))
+          return True
+        else:
+          print("unknown href: {0}".format(href))
+      except Exception as e:
+        sublime.error_message("Phatom navigation error\nactive_phantom_ids: {0}\nhref: {1}\nerror: {2}".format(active_phantom_ids, href, e))
+    return False
 
 class QCloseLastPhantomCommand(q_chain.QChainCommand):
 
   def do(self, edit, input=None):
     #only remove phantom if no output panel
-    panel = self.view.window().find_output_panel("q")
     output_showing = self.view.window().active_panel() == "output.q"
     if not output_showing:
-      phantoms = self.view.settings().get('phantoms')
-      if phantoms:
-        last_phantom = phantoms.pop()
+      active_phantom_ids = self.view.settings().get('active_phantom_ids')
+      if active_phantom_ids:
+        last_phantom = active_phantom_ids.pop()
         self.view.erase_phantoms(last_phantom)
-        self.view.settings().set('phantoms', phantoms)
+        self.view.settings().set('active_phantom_ids', active_phantom_ids)
     return '' #return something so q_chain may continue
 
 #show output in phantom
