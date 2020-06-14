d562a178c1713e2ade53828e8d2a8c74900cdfa0
atf1206 <atf1206@gmail.com>
2020-06-14 15:37:26 -0400

    Cleanup send, expand error handling

--- q_send.py
+++ q_send.py
@@ -26,64 +26,85 @@ class QSendRawCommand(q_chain.QChainCommand):
 
     #keep this because it is overwritten in QUpdateCompletionsCommand
     def send(self, con, input):
-      return QSendRawCommand.sendAndUpdateStatus(self.view, con, input)
+        return QSendRawCommand.sendAndUpdateStatus(self.view, con, input)
 
     @staticmethod
     def sendAndUpdateStatus(view, con, input):
-      view.set_status('result', 'executing...')
-      try:
-        d = QSendRawCommand.executeRaw(con, input)
-        view.set_status('result', d['status'])
-        view.set_status('q', con.status())
-        return d['result']
-      except Exception as e:
-        sublime.error_message('Error in QSendRawCommand.sendAndUpdateStatus:\n' + str(e))
-        view.set_status('result', 'ERROR')
-        view.set_status('q', con.status())
-        raise e
+        view.set_status('result', 'executing...')
+        try:
+            d = QSendRawCommand.executeRaw(con, input)
+            view.set_status('result', d['status'])
+            view.set_status('q', con.status())
+            return d['result']
+        except Exception as e:
+            sublime.error_message('Error in QSendRawCommand.sendAndUpdateStatus:\n' + str(e))
+            view.set_status('result', 'ERROR')
+            view.set_status('q', con.status())
+            raise e
 
     @staticmethod
     def executeRaw(con, input):
-      try:
-          q = con.q
-          q.open()
+        try:
+            q = con.q
+            q.open()
 
-          start_time = datetime.datetime.now()
+            input_prefix = input[:5] # probably .Q.s or .j.j -- make more generic later if necessary
+            input_body = input[5:]
 
-          write_flag = q('@[{`.st.tmp set x;1b};();0b]')
-          input = input[:5] + ('.st.tmp:' + input[5:] if write_flag else input[5:])
+            mem_query = '@[{.Q.w[][`used]};();0]'
+            write_query = '@[{`.st.tmp set x;`sublimeq};();0b]'
+            initial_query = '(' + mem_query + ';' + write_query + ')'
 
-          mem = '@[{.Q.w[][`used]};();0]'
-          dims = '$[@[{`tmp in key x};`.st;0b];" x " sv string (count @[{$[0<=type x; cols x;()]};.st.tmp;()]),count .st.tmp;0]'
-          res = q('(' + ';'.join([dims, mem, input, mem]) + ')')
+            pre_res = q(initial_query)
 
-          end_time = datetime.datetime.now()
-          time = str(end_time - start_time)[2:-3]
+            try:
+                write_flag = util.decode(pre_res[1]) == 'sublimeq'
+                start_mem = int(util.decode(pre_res[0]))
+            except:
+                write_flag = False
+                start_mem = 0
 
-          count, mem, res = [util.decode(x) for x in [res[0], res[1] - res[3], res[2]]]
-          mem = int(mem)
-          sign = '+' if mem>0 else '-'
-          mem = util.format_mem(abs(mem))
+            input =  input_prefix + ('.st.tmp:' if write_flag else '') + input_body
 
-          #return input itself if query is define variable or function (and return no result)
-          if res is None:
-              res = input
+            start_time = datetime.datetime.now()
+            res = util.decode(q(input))
+            end_time = datetime.datetime.now()
+            time = str(end_time - start_time)[2:-3]
 
-          status = 'Result: ' + count + ', ' + time + ', ' + sign + mem
-          #self.view.set_status('result', 'Result: ' + count + ', ' + time + ', ' + sign + mem)
-      except QException as e:
-          res = "error: `" + util.decode(e)
-          status = "error: `" + util.decode(e)
-      except socket_error as serr:
-          msg = 'Sublime-q cannot to connect to \n"' + con.h() + '"\n\nError message: ' + str(serr)
-          sublime.error_message(msg)
-          res = ""
-          status = "error: " + str(serr)
-      finally:
-          q.close()
+            dims_query = '$[@[{`tmp in key x};`.st;0b];" x " sv string (count @[{$[0<=type x; cols x;()]};.st.tmp;()]),count .st.tmp;0]'
+            post_query = '(' + mem_query + ';' + dims_query + ')'
+            post_res = q(post_query)
 
-      #self.view.set_status('q', con.status())
-      return {'result': res, 'status': status}
+            try:
+                end_mem = int(util.decode(post_res[0]))
+                mem = end_mem - start_mem
+                sign = '+' if mem>0 else '-'
+                mem = util.format_mem(abs(mem))
+                dims = util.decode(post_res[1])
+            except:
+                sign = ''
+                mem = '0'
+                dims = '0'
+
+            # Return input itself if no results return (e.g. a query that defines a variable or function)
+            # if res in [None, 'None']:
+            #     res = input_body
+
+            status = 'Result: ' + dims + ', ' + time + ', ' + sign + mem
+            #self.view.set_status('result', 'Result: ' + count + ', ' + time + ', ' + sign + mem)
+        except QException as e:
+            res = "error: `" + util.decode(e)
+            status = "error: `" + util.decode(e)
+        except socket_error as serr:
+            msg = 'Sublime-q cannot to connect to \n"' + con.h() + '"\n\nError message: ' + str(serr)
+            sublime.error_message(msg)
+            res = ""
+            status = "error: " + str(serr)
+        finally:
+            q.close()
+
+        #self.view.set_status('q', con.status())
+        return {'result': res, 'status': status}
 
 class QSendCommand(QSendRawCommand):
     def do(self, edit=None, input=None):
@@ -100,5 +121,3 @@ class QSendJsonCommand(QSendRawCommand):
 
         input = ".j.j " + input  #save to temprary result, so we can get dimension later
         return super().do(input=input)
-
-
